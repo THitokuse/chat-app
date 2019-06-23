f00c42c3fed87982e7b457077ac5c67474efab5c
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-24 01:40:57 +0900

    add export components page in index.js

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,1 +1,22 @@
+// Top
+export { default as TopFaceScreen } from "./TopScreen/TopFaceScreen";
+export { default as TopPersonalityScreen } from "./TopScreen/TopPersonalityScreen";
+
+// Mypage
 export { default as MypageScreen } from "./MypageScreen/MypageScreen";
+export { default as SettingScreen } from "./MypageScreen/SettingScreen";
+export { default as ProfileEditScreen } from "./MypageScreen/ProfileEditScreen";
+
+// Column
+export { default as ColumnIndexScreen } from "./ColumnScreen/ColumnIndexScreen";
+export { default as ColumnScreen } from "./ColumnScreen/ColumnScreen";
+
+// Message
+export { default as MypageScreen } from "./MessageScreen/MessageIndexScreen";
+export { default as MypageScreen } from "./MessageScreen/MessageScreen";
+export { default as MypageScreen } from "./MessageScreen/TimelineScreen";
+
+// PersonalityCheck
+export { default as PersonalityCheckScreen } from "./PersonalityCheckScreen/PersonalityCheckScreen";
+export { default as PersonalityCheckConfirmScreen } from "./PersonalityCheckScreen/PersonalityCheckConfirmScreen";
+export { default as PersonalityCheckResultScreen } from "./PersonalityCheckScreen/PersonalityCheckResultScreen";
