7a5f551a076457295e7083ecc844e2b600e0bc1f
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-23 19:07:11 +0900

    add Top Screen which are Face Screen and Personality Screen.

---
+++ src/components/pages/TopScreen/TopFaceScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>Top Face Screen</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "TopFaceScreen"
+};
+</script>
+
+<style>
+.container { 
+  flex: 1;
+  margin-bottom: 30;
+}
+</style>
---
+++ src/components/pages/TopScreen/TopPersonalityScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>Top Personality Screen</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "TopPersonalityScreen"
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  margin-bottom: 30;
+}
+</style>
