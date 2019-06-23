f826838d82e7a75069019baa27dd45f7b989a2f6
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-24 01:40:22 +0900

    add PersonalityCheckConfirm Screen.

---
+++ src/components/pages/PersonalityCheckScreen/PersonalityCheckConfirm.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>Personality Check Confirm Screen</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "PersonalityCheckConfirmScreen"
+};
+</script>
+
+<style>
+.container { 
+  flex: 1;
+  margin-bottom: 30;
+}
+</style>
