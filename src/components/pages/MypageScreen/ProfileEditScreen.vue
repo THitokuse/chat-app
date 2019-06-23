e3cba332adb210916d8ccc48765a26bf290314fa
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-24 01:39:53 +0900

    add Mypage Screen which are ProfileEdit and Setting Screen.

---
+++ src/components/pages/MypageScreen/ProfileEditScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>Profile Edit Screen</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "ProfileEditScreen"
+};
+</script>
+
+<style>
+.container { 
+  flex: 1;
+  margin-bottom: 30;
+}
+</style>
