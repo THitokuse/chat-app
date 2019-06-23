c354010c714fe20ee4ec69daf70dcddec49641bf
THitokuse <hasegawa.taichiro@gmail.com>
2019-06-23 19:08:41 +0900

    add Message Screen which are MessageIndex Screen and Message Screen.

---
+++ src/components/pages/MessageScreen/MessageScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>Message Screen</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "MessageScreen"
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  margin-bottom: 30;
+}
+</style>
---
+++ src/components/pages/MessageScreen/TimelineScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="container">
+    <text>Timeline Screen</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "TimelineScreen"
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  margin-bottom: 30;
+}
+</style>
