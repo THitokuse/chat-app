48f58b205a7a169325d3564248553af0280f3cb7
shogo-takao <taka.sho2508@gmail.com>
2019-06-26 20:26:27 +0900

    wip

--- App.vue
+++ App.vue
@@ -1,8 +1,18 @@
 <template>
   <view class="container">
     <text class="text-color-primary">My Vue Native App</text>
+    <button />
+    <button disabled />
     </view>
 </template>
+
+<script>
+import { Button } from "./src/components/atoms";
+export default {
+  components: { Button }
+};
+</script>
+
  
 <style>
 .container {
---
+++ src/components/atoms/Button/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <view class="button-wrapper">
+    <!-- <view v-if="disabled" class="disabled">
+      <text>Disabled</text>
+    </view> -->
+    <touchable-opacity
+      :class="[
+        type ? type : '',
+        disabled ? 'disable' : '',
+      ]"
+      @click="onPress"
+    >
+      <text>Button</text>
+    </touchable-opacity>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    type: {
+      type: String,
+      default: ""
+    },
+    onPress: {
+      type: Function,
+      default: function() {}
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style>
+.button-wrapper {
+  line-height: 1.4;
+  padding: 5px 15px;
+  margin-top: 8px;
+  border-radius: 4px;
+}
+.disable {
+  opacity: 0.3;
+}
+</style>
