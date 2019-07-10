995aebb8fdf0b0752bda2954a1aa25d74bb7f0c9
THitokuse <hasegawa.taichiro@gmail.com>
2019-07-11 01:36:45 +0900

    create DescriptionArea Atom Component

---
+++ src/components/atoms/DescriptionArea/DescriptionArea.vue
@@ -0,0 +1,17 @@
+<template>
+  <text class="description">
+    記事テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト
+  </text>  
+</template>
+
+<script>
+export default {
+  name: "DescriptionArea"
+}
+</script>
+
+<style>
+.description {
+  padding-horizontal: 40;
+}
+</style>
