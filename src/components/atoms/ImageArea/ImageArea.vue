f5df464569b85e52392809198ba85211e3bd10c3
THitokuse <hasegawa.taichiro@gmail.com>
2019-07-11 01:37:11 +0900

    create ImageArea atom Component

---
+++ src/components/atoms/ImageArea/ImageArea.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="column-image">
+    <image
+      :style="{ width: 300, height: 250}"
+      resizeMode='contain'
+      :source="{uri:uri}"
+    />
+  </view>
+</template>
+
+<script>
+export default {
+  name: "ImageArea",
+  props: ["uri"]
+}
+</script>
+
+<style>
+.column-image {
+  padding-horizontal: 50;
+}
+</style>
