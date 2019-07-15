5b4782c2b6a151888e0897376b476277281ec8b0
THitokuse <hasegawa.taichiro@gmail.com>
2019-07-15 13:17:10 +0900

    make Search Field into Header

---
+++ src/components/molecles/SearchField/SearchField.vue
@@ -0,0 +1,23 @@
+<template>
+  <view class="search-field">
+    <IconContainer></IconContainer>
+    <text-input
+      placeholder="絞り込み"
+    ></text-input>
+  </view>
+</template>
+
+<script>
+import IconContainer from '../../atoms/Button/HorizontalButton/IconContainer'
+import TextField from '../../atoms/TextField/TextField'
+
+export default {
+  name: "SearchField",
+  components: {
+    IconContainer,
+    TextField
+  }
+}
+</script>
+
+<style></style>
