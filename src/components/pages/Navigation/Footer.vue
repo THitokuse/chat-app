6b81dcff06faa868d652c1012bee542427f398a4
IshinShibata <kobukobudesu.dekaihou@gmail.com>
2019-07-20 11:46:17 +0900

    カード変更

---
+++ src/components/pages/Navigation/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <app-navigation></app-navigation>
+</template>
+
+<script>
+import { StackNavigator } from "vue-native-router";
+const AppNavigation = StackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+export default {
+    components: { AppNavigation }
+}
+</script>
--- src/components/pages/TopScreen/TopFaceScreen.vue
+++ src/components/pages/TopScreen/TopFaceScreen.vue
@@ -1,15 +1,17 @@
 <template>
   <default-template>
     <text>Top Face Screen</text>
+    <card-component />
   </default-template>
 </template>
 
 <script>
 import { DefaultTemplate } from "../../templates";
+import CardComponent from "./CardComponent";
 
 export default {
   name: "TopFaceScreen",
-  components: { DefaultTemplate }
+  components: { DefaultTemplate, CardComponent }
 };
 </script>
 
